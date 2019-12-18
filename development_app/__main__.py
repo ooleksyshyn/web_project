259cbb85b5b3b0e410d7b314a655764ab9c9fbf7
Oleksii <o_oleksyshyn@ukr.net>
2019-12-18 19:50:20 +0200

    bug fixes

--- development_app/__main__.py
+++ development_app/__main__.py
@@ -4,3 +4,14 @@ import development_app.view
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+# TODO: add salary and birth date for employees, average salary for department
+
+# TODO: add EDIT and DELETE functions for departments and employees
+
+# TODO: add search for employees with certain birth date, and range of birth dates
+
+# TODO: write documentation
+
+# TODO: write tests
