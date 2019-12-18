d1610627486d29f323f8a36468bbdae270660ebd
Oleksii <o_oleksyshyn@ukr.net>
2019-12-18 21:40:40 +0200

    Basic test added

---
+++ tests/test_basic.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class BasicTest(unittest.TestCase):
+    def setUp(self):
+        return super().setUp()
+
+    def test_basic(self):
+        ...
+
+
+if __name__ == '__main__':
+    unittest.main()
